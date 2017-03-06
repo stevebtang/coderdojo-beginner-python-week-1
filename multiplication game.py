ab7f0a2226d0145bbe5fefdaccadabd14e043688
Steve Tangsombatvisit <stang@netflix.comm>
2017-03-06 08:22:14 -0800

    support python 3

--- multiplication game.py
+++ multiplication game.py
@@ -9,11 +9,11 @@ number2 = 1
 answer = number1 * number2
 
 guess = 0
-print "What is", number1, "x", number2, "?"
+print ("What is", number1, "x", number2, "?")
 
 while guess != answer:
-    guess = input("Answer: ")
+    guess = int(input("Answer: "))
     if guess != answer:
-        print "No, try again"
+        print ("No, try again")
 
-print "You got it!"
+print ("You got it!")
